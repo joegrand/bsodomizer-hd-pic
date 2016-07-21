7302684b37758b517b6ef5a944fa244d55a9ed2c
Joe Grand <joe@grandideastudio.com>
2016-07-21 00:21:14 -0700

    Adjusted configuration bits (enable brown out reset)

--- BSOD_Main.h
+++ BSOD_Main.h
@@ -15,7 +15,7 @@
 #ifndef __BSOD_MAIN_H__
 #define __BSOD_MAIN_H__
 
-#define __BSOD_DEBUG             // #define for debug capability
+#define __BSOD_DEBUG            // #define for debug capability
 #define _XTAL_FREQ  4000000     // oscillator frequency for __delay_xx() macros
 
 /**************************************************************************
@@ -28,7 +28,7 @@
 #pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
 #pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
 #pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
-#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
 #pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
 #pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
@@ -43,7 +43,7 @@
 #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
 #pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
 #pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config BORV = HI        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), high trip point selected, 2.7V)
 #pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
 
 
